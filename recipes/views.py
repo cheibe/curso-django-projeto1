2e455fc64db306fbe65cfd72db5f59fb809a1d9e
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2023-11-22 17:32:00 -0300

    incio

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home1')
+    return render(request, 'recipes/home.html')
 
-def sobre (request):
-    return HttpResponse('sobre')
-
-def contato (request):
-    return HttpResponse('contato')
