40676b84ac25794aad76471ec1db9c5bae092a9a
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2023-11-27 13:23:21 -0300

    creat recipe and category

--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -25,5 +27,10 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
 
     
