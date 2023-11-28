9bcd90b186bf39842f39acc296b04d8331836acf
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2023-11-28 17:31:39 -0300

    get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -16,7 +16,15 @@ def recipe(request, id):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}  - CATEGORY |'
     })
