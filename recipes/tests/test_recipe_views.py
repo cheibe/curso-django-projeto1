c47889ea4a588213c8d7fa34c8706f69c44588a6
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2023-12-05 10:11:32 -0300

    tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -31,6 +31,10 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('5 minutos', content)
         self.assertEqual(len(response_context_recipes), 1)
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('<h1>NO RECIPES FOUND HERE 😢</h1>', response.content.decode('utf-8'))
 
     def test_recipe_category_view_fuction_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
@@ -40,12 +44,40 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_load_recipe(self):
+        title = 'receita teste cetegory'
+        self.make_recipe(title=title)
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))
+        content = response.content.decode('utf-8')
+        self.assertIn(title, content)
+        self.assertIn('1 pessoa', content)
+        self.assertIn('5 minutos', content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_fuction_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 2}))
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_detail_view_returns_404_if_no_recipes(self):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': 500}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_template_load_the_correct_recipe(self):
+        title = 'receita teste detail'
+        self.make_recipe(title=title)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        content = response.content.decode('utf-8')
+        self.assertIn(title, content)
+        self.assertIn('1 pessoa', content)
+        self.assertIn('5 minutos', content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
         self.assertEqual(response.status_code, 404)
 
     
