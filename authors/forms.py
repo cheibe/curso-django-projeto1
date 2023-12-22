7d354b882dbb77df5143ea2be04771c434c28651
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2023-12-22 14:19:58 -0300

    overwriting the fields

--- authors/forms.py
+++ authors/forms.py
@@ -1,7 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Your name')
+        add_placeholder(self.fields['last_name'], 'Your surname')
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
     class Meta:
         model = User
         fields = [
@@ -11,3 +32,9 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        widgets = {
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
